9ba6e35e178ab1688804a70ecebadfca0095e849
wallacegibbon <wallacegibbon@aliyun.com>
2023-03-28 22:31:09 +0800

    Bug fix: The preprocessor commands for checking chip type was wrong. `if defined` should be used.

--- ch32v/sc_ch32v_i2c.h
+++ ch32v/sc_ch32v_i2c.h
@@ -1,9 +1,11 @@
 #ifndef __SC_CH32V_I2C_H
 #define __SC_CH32V_I2C_H
 
-#ifdef CHIP_CH32V10X
+#if defined(CHIP_CH32V00X)
+#include "ch32v00x_i2c.h"
+#elif defined(CHIP_CH32V10X)
 #include "ch32v10x_i2c.h"
-#elif CHIP_CH32V30X
+#elif defined(CHIP_CH32V30X)
 #include "ch32v30x_i2c.h"
 #else
 #include "ch32v20x_i2c.h"
