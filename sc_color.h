f490bce5de01564de7733b51cfc848d132ea696c
wallace <wallacegibbon@aliyun.com>
2023-12-20 15:31:38 +0800

    Trivial improvements: Color literal (24 bit) macros are calibrated by Wikipedia.

--- sc_color.h
+++ sc_color.h
@@ -11,23 +11,21 @@ static inline uint8_t color_to_1bit(uint32_t color) {
 	return color ? 1 : 0;
 }
 
+/// These color values are copied from Wikipedia.
 #define WHITE_24bit 0xFFFFFF
 #define BLACK_24bit 0x000000
+#define GRAY_24bit 0x808080
+#define LIGHTGRAY_24bit 0xC0C0C0
 #define RED_24bit 0xFF0000
 #define GREEN_24bit 0x00FF00
 #define BLUE_24bit 0x0000FF
 #define CYAN_24bit 0x00FFFF
 #define MAGENTA_24bit 0xFF00FF
 #define YELLOW_24bit 0xFFFF00
-#define BROWN_24bit 0xB88800
-#define BRRED_24bit 0xF88038 /// maroon
-#define GRAY_24bit 0x808480
-#define DARKBLUE_24bit 0x003878
-#define LIGHTBLUE_24bit 0x78CCE0
-#define GRAYBLUE_24bit 0x5088C0
-#define LIGHTGREEN_24bit 0x8080FF
-#define LGRAY_24bit 0xC0C0C0
-#define LGRAYBLUE_24bit 0xA0C888
-#define LBBLUE_24bit 0x286090
+#define BROWN_24bit 0x7F3F00
+#define MAROON_24bit 0x800000
+#define DARKBLUE_24bit 0x00008B
+#define LIGHTBLUE_24bit 0xADD8E6
+#define LIGHTGREEN_24bit 0x90EE90
 
 #endif
